ed85ec4d1b5afb247e4ece4dcbb9b664a2109874
Marcin Wójcik <marcin.wojcik@getresponse.com>
2018-11-14 23:19:40 +0100

    scoring server

--- src/App.js
+++ src/App.js
@@ -36,41 +36,29 @@ class App extends Component {
 
             if (!auth) return;
 
-            const playerProfile = Auth0.getProfile();
-            const currentPlayer = {
-                id: playerProfile.sub,
+            self.playerProfile = Auth0.getProfile();
+            self.currentPlayer = {
+                id: self.playerProfile.sub,
                 maxScore: 0,
-                name: playerProfile.name,
-                picture: playerProfile.picture,
+                name: self.playerProfile.name,
+                picture: self.playerProfile.picture,
             };
 
-            this.props.loggedIn(currentPlayer);
+            this.props.loggedIn(self.currentPlayer);
 
-            const socket = io('http://localhost:3001', {
+            self.socket = io('http://localhost:3001', {
                 query: `token=${Auth0.getAccessToken()}`,
             });
 
-            let emitted = false;
-
-            socket.on('players', (players) => {
+            self.socket.on('players', (players) => {
                 this.props.leaderboardLoaded(players);
 
-                if (emitted) return;
-                socket.emit('new-max-score', {
-                    id: playerProfile.sub,
-                    maxScore: 120,
-                    name: playerProfile.name,
-                    picture: playerProfile.picture,
-                });
-                emitted = true;
-                setTimeout(() => {
-                    socket.emit('new-max-score', {
-                        id: playerProfile.sub,
-                        maxScore: 222,
-                        name: playerProfile.name,
-                        picture: playerProfile.picture,
-                    });
-                }, 5000);
+                players.forEach(player => {
+                    if (player.id === self.currentPlayer.id) {
+                        self.currentPlayer.maxScore = player.maxScore;
+                    }
+                })
+
             });
 
         });
@@ -85,6 +73,19 @@ class App extends Component {
             cnv.style.height = `${window.innerHeight}px`;
         };
         window.onresize();
+
+    }
+
+    componentWillReceiveProps(newProps) {
+
+        if (!newProps.gameState.started && this.props.gameState.started) {
+            if (this.props.gameState.kills > this.currentPlayer.maxScore) {
+                this.socket.emit('new-max-score', {
+                    ...this.currentPlayer,
+                    maxScore: this.props.gameState.kills,
+                });
+            }
+        }
 
     }
 
